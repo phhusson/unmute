0690713f4f003474aecc9433c8368950368be7d8
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2025-06-12 08:45:22 +0000

    Put back the bake_deploy_staging.sh

---
+++ bake_deploy_staging.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+export DOMAIN=unmute-staging.kyutai.io
+export LLM_MODEL=google/gemma-3-1b-it
+export DOCKER_HOST=ssh://root@${DOMAIN}
+
+echo "If you get an connection error, do: ssh root@${DOMAIN}"
+
+docker buildx bake -f ./swarm-deploy.yml --allow=ssh --push
+docker stack deploy --with-registry-auth --compose-file ./swarm-deploy.yml llm-wrapper

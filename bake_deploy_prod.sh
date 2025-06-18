b495974f9edac3f08fe639c44a75d642ed642463
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2025-06-18 12:52:51 +0000

    Add prune when deploying to easily change services names

--- bake_deploy_prod.sh
+++ bake_deploy_prod.sh
@@ -21,4 +21,4 @@ export DOCKER_HOST=ssh://root@${DOMAIN}
 echo "If you get an connection error, do: ssh root@${DOMAIN}"
 
 docker buildx bake -f ./swarm-deploy.yml --allow=ssh --push
-docker stack deploy --with-registry-auth --compose-file ./swarm-deploy.yml llm-wrapper
+docker stack deploy --with-registry-auth --prune --compose-file ./swarm-deploy.yml llm-wrapper
